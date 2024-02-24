e4eba585244064d0502bc5b89fac7b98cc75cf2c
iWasBornYoung-45 <ap.pande.02@gmail.com>
2024-02-24 13:26:48 +0530

    added cors

--- main.py
+++ main.py
@@ -3,9 +3,20 @@ from dotenv import main
 import models
 from database import engine
 from routes import posts, users, votes
+from fastapi.middleware.cors import CORSMiddleware
+
 models.Base.metadata.create_all(bind=engine)
+origins = ["*"]
 
 app = FastAPI(title="postIt!", description="REST services for postIt!")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
+
 main.load_dotenv()
 
 app.include_router(posts.router)
--- routes/posts.py
+++ routes/posts.py
@@ -1,5 +1,5 @@
 import models, schemas, utils
-from fastapi import status, HTTPException, Depends, APIRouter
+from fastapi import status, HTTPException, Depends, APIRouter, Query
 from sqlalchemy.orm import Session
 from database import get_db
 from uuid import UUID
@@ -9,15 +9,16 @@ from sqlalchemy import func
 router = APIRouter(prefix='/posts', tags=['Posts'])
 
 @router.get('/', response_model = list[schemas.PostsResponseBody])
-async def get_posts(db: Annotated[Session, Depends(get_db)], id: UUID | None  = None, limit: int | None = 5, skip: int | None = 0, search: str | None = ""):
+async def get_posts(db: Annotated[Session, Depends(get_db)], limit: Annotated[int | None, Query(le=10)] = 5, id: UUID | None  = None, skip: int | None = 0, search: str | None = ""):
+    query = db.query(models.Posts, func.count(models.Upvotes.post_id).label("upvote_count")).join(models.Upvotes, models.Posts.id == models.Upvotes.post_id, isouter=True)
     if not id:
-        query = db.query(models.Posts, func.count(models.Upvotes.post_id).label("upvote_count")).join(models.Upvotes, models.Posts.id == models.Upvotes.post_id, isouter=True).filter(models.Posts.published == True, models.Posts.title.contains(search)).group_by(models.Posts.id).limit(limit).offset(skip)
+        query = query.filter(models.Posts.published == True, models.Posts.title.contains(search)).group_by(models.Posts.id).limit(limit).offset(skip)
     else:
-        query = db.query(models.Posts, func.count(models.Upvotes.post_id).label("upvote_count")).join(models.Upvotes, models.Posts.id == models.Upvotes.post_id, isouter=True).filter(models.Posts.published == True, models.Posts.id == id).group_by(models.Posts.id)
+        query = query.filter(models.Posts.published == True, models.Posts.id == id).group_by(models.Posts.id)
     res = query.all()
     res = sorted([{'post': post, 'upvotes': upvotes} for post, upvotes in res], key=lambda x:x['upvotes'], reverse=True)
     return res
-   
+
 @router.post('/', response_model=schemas.ResponseBody, status_code=status.HTTP_201_CREATED)
 async def add_post(payload: schemas.BodySchema, db: Annotated[Session, Depends(get_db)], current_user: Annotated[models.Users, Depends(utils.get_current_user)]):
     print(current_user.user_id)
